686465330877e424480135fba719b864c4f2bba0
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-07-26 12:03:21 +0300

    chore: Update StudentDashboard component to display subjects with images

--- app/dashboard/_components/StudentDashboard.tsx
+++ app/dashboard/_components/StudentDashboard.tsx
@@ -9,10 +9,16 @@ const StudentDashboard: React.FC<StudentDashboardProps> = ({ grade }) => {
   const gradeInfo = grades.find(g => g.grade === grade);
 
   return (
-    <div>
-      <h1>Student Dashboard</h1>
-      <p>Grade: {grade}</p>
-      {gradeInfo && <p>{gradeInfo.subjects.map(subject => subject.name).join(', ')}</p>}
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 justify-around lg:ml-10">
+      {gradeInfo && gradeInfo.subjects.map(subject => (
+        <div key={subject.name} className="lg:w-[15vw] lg:h-[40vh] m-2 border border-gray-300 rounded-lg shadow-lg flex flex-col items-center text-center mt-8 mb-6  hover:scale-110 transition-transform duration-400">
+          <img src={subject.image} alt={subject.name} className="w-full h-3/4 object-cover rounded-t-lg" />
+          <div className="p-2">
+            <h2 className="text-xl font-semibold">{subject.name}</h2>
+            <p className="text-gray-700 ">{subject.teacher}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
