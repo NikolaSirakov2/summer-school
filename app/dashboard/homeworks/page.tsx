1789edde86be3720ce2900b9d12b95d1f2aaa139
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-07-28 11:36:10 +0300

    Update the look of Subject and Homeworks pages

--- app/dashboard/homeworks/page.tsx
+++ app/dashboard/homeworks/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React from 'react';
 import { schoolSubjects } from '@/data/schoolSubjects';
+import { Button } from '@/components/ui/button';
 
 const subjects: {
   id: number;
@@ -14,6 +15,7 @@ const Card = ({ subject }: { subject: { id: number; name: string; homework: stri
   <div className="border-2 shadow-xl rounded-xl p-4 flex flex-col m-4 hover:bg-gray-200">
     <span className='text-2xl font-bold mb-4'>{subject.name}</span>
     <span>{subject.homework}</span>
+    <Button className="mt-4">Предай домашно</Button>
   </div>
 );
 
@@ -22,10 +24,17 @@ const HomeworksDashboard = () => {
   const subjects = schoolSubjects.find(subject => subject.grade === studentGrade)?.subjects;
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-      {subjects && subjects.map(subject => (
-        <Card key={subject.id} subject={subject} />
+    <div className="grid grid-cols-1 lg:grid-cols-3 lg:grid-rows-4 gap-4">
+      {subjects && subjects.map((subject, index) => (
+        <div
+          key={subject.id}
+        >
+          <Card subject={subject} />
+        </div>
       ))}
+      <div className="hidden lg:flex lg:col-span-1 lg:row-span-2 lg:col-start-3 lg:row-start-3 justify-center items-center">
+        <img src="/school-boy.avif" alt="Additional Image" className="lg:w-[25vw] object-cover" />
+      </div>
     </div>
   );
 }
--- app/dashboard/subject/page.tsx
+++ app/dashboard/subject/page.tsx
@@ -1,18 +1,24 @@
 'use client';
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { schoolSubjects } from "../../../data/schoolSubjects";
 
 const SubjectDetails: React.FC = () => {
+  const [role, setRole] = useState<string | null>('');
   const searchParams = useSearchParams();
   const grade = searchParams.get('grade');
   const subjectId = searchParams.get('subject');
+
+  useEffect(() => {
+    const storedRole = localStorage.getItem('role');
+    setRole(storedRole);
+  }, []);
 
   const gradeData = schoolSubjects.find(g => g.grade === grade);
   const subjectData = gradeData?.subjects.find(s => s.id.toString() === subjectId);
 
   return (
-    <div className="flex flex-col lg:flex-row">
+    <div className="relative flex flex-col lg:flex-row">
       {subjectData ? (
         <>
           <div className="w-full lg:w-[40vw] h-[85vh] ml-10 mt-10">
@@ -21,14 +27,23 @@ const SubjectDetails: React.FC = () => {
           <div className="flex-1 p-4 ml-10 mt-10">
             <h1 className="text-4xl font-bold">{subjectData.name}</h1>
             <h3 className="text-2xl mt-4">{subjectData.teacher}</h3>
-            <div className="mt-8">
-              <h4 className="text-lg font-semibold">Оценки:</h4>
-              <p>5, 4, 6</p>
+            <div className="mt-8 lg:w-1/2 border rounded-lg shadow-lg">
+              <h4 className="text-lg font-semibold m-4">Оценки:</h4>
+              <p className="m-4">5, 4, 6</p>
             </div>
-            <div className="mt-8">
-              <h4 className="text-lg font-semibold">Задачи за домашна работа:</h4>
-              <p>{subjectData.homework}</p>
+            <div className="mt-8 lg:w-1/2 border rounded-lg shadow-lg">
+              <h4 className="text-lg font-semibold m-4">Задачи за домашна работа:</h4>
+              <p className="m-4">{subjectData.homework}</p>
             </div>
+            {role === 'parent' && (
+              <div className="mt-8 lg:w-1/2 border rounded-lg shadow-lg">
+                <h4 className="text-lg font-semibold m-4">Бележки:</h4>
+                <p className="m-4">Тук учителя може да добави бележки за вашето дете.</p>
+              </div>
+            )}
+          </div>
+          <div className="absolute bottom-4 right-4 lg:w-[20vw]">
+            <img src="/boy-girl.webp" alt="Additional Image" className="w-full h-full object-cover" />
           </div>
         </>
       ) : (
